d0b8f68d34fd43fb2aa11df7c25a5d3a769de07d
ds5105119 <ds5105119@gmail.com>
2025-01-13 03:37:37 +0900

    login 로직 구현

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -12,9 +12,10 @@ export default function LoginPage() {
     <div className="container flex w-full h-full flex-col items-center justify-center">
       <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
         <div className="flex flex-col space-y-2 text-center">
-          <Image src="/logo.svg" alt="로고" width={50} height={50} className="mx-auto" />
-          <h1 className="text-2xl font-semibold tracking-tight">올집</h1>
-          <p className="text-sm text-muted-foreground">로그인하여 개인 맞춤형 복지 정책 정보를 확인하세요</p>
+          <span>
+            <h1 className="text-2xl font-extrabold tracking-tight text-violet-700">올집</h1>
+          </span>
+          <p className="text-sm text-muted-foreground">로그인하여 신청할 수 있는 모든 복지 정보를 확인하세요</p>
         </div>
         <LoginForm />
       </div>
--- src/components/form/loginform.tsx
+++ src/components/form/loginform.tsx
@@ -5,7 +5,7 @@ import { useFormStatus } from "react-dom";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Loader2 } from "lucide-react";
+import { Eye, EyeOff, Loader2 } from "lucide-react";
 import { GoogleIcon, KakaoIcon } from "@/components/icon/loading";
 import { Toast } from "@/components/toast/toast";
 import { login } from "@/hooks/account/user";
@@ -28,26 +28,53 @@ function SubmitButton() {
 export default function LoginForm() {
   const [toastMessage, setToastMessage] = useState<string | null>(null);
   const [state, formAction] = useActionState(login, initialState);
+  const [showPassword, setShowPassword] = useState(false);
+  const [email, setEmail] = useState("");
 
   useEffect(() => {
     console.log(state);
   }, [state]);
 
   return (
-    <div className="grid gap-6">
+    <div className="grid gap-5">
       <form action={formAction}>
-        <div className="grid gap-2">
+        <div className="grid gap-3">
           <div className="grid gap-1">
             <Label className="sr-only" htmlFor="email">
               이메일
             </Label>
-            <Input id="email" name="email" placeholder="name@example.com" type="email" autoCapitalize="none" autoComplete="email" autoCorrect="off" />
+            <Input
+              id="email"
+              name="email"
+              placeholder="이메일 주소"
+              type="email"
+              autoCapitalize="none"
+              autoComplete="email"
+              autoCorrect="off"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
           </div>
-          <div className="grid gap-1">
+          <div className="grid gap-1 relative">
             <Label className="sr-only" htmlFor="password">
               비밀번호
             </Label>
-            <Input id="password" name="password" placeholder="비밀번호" type="password" autoCapitalize="none" autoComplete="current-password" autoCorrect="off" />
+            <Input
+              id="password"
+              name="password"
+              placeholder="비밀번호"
+              type={showPassword ? "text" : "password"}
+              autoCapitalize="none"
+              autoComplete="current-password"
+              autoCorrect="off"
+            />
+            <button
+              type="button"
+              onClick={() => setShowPassword(!showPassword)}
+              className="absolute right-3 top-1/2 transform -translate-y-1/2"
+            >
+              {showPassword ? <EyeOff className="h-4 w-4 text-gray-500" /> : <Eye className="h-4 w-4 text-gray-500" />}
+            </button>
           </div>
           <SubmitButton />
         </div>
@@ -57,18 +84,21 @@ export default function LoginForm() {
           <span className="w-full border-t" />
         </div>
         <div className="relative flex justify-center text-xs uppercase">
-          <span className="bg-background px-2 text-muted-foreground">소셜 로그인</span>
+          <span className="bg-background px-2 text-muted-foreground">또는</span>
         </div>
       </div>
-      <div className="grid gap-2">
+      <div className="grid gap-3">
         <Button variant="outline" type="button" className="bg-white text-black hover:bg-gray-100">
           <GoogleIcon className="mr-2 h-4 w-4" />
           Google로 로그인
         </Button>
-        <Button variant="outline" type="button" className="bg-[#FEE500] text-black hover:bg-[#FDD000]">
+        <Button variant="secondary" type="button" className="bg-[#FEE500] text-black hover:bg-[#FDD000]">
           <KakaoIcon className="mr-2 h-4 w-4" />
-          카카오로 로그인
+          카카오로 3초만에 시작하기
         </Button>
+      </div>
+      <div className="relative flex justify-center text-xs uppercase">
+        <span className="bg-background px-2 text-muted-foreground text-violet-700">회원가입</span>
       </div>
       {toastMessage && <Toast message={toastMessage} onClose={() => setToastMessage(null)} />}
     </div>
